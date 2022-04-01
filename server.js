9a39cd69bd887236ec8b5f44121fde53377f1bbf
Noct <ezy_95@outlook.com>
2022-04-02 03:29:04 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('./client/build'))
+
+app.get('/generate', (req, res) => {
+	res.status(200).json({ generate: 'Successfull' })
+})
+
+app.get('/api/v1', (req, res) => {
+	res.status(200).json({ msg: 'This is API v1' })
+})
+
+app.all('*', (req, res) => {
+	res.status(404).send('Not found')
+})
+
+const port = 5000
+
+const start = () => {
+	app.listen(port, console.log(`Server is listening at port: ${port}`))
+}
+
+start()
