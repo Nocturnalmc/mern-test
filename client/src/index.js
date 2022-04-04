011783a570e00db9f712a9c0a46025d457488818
Noct <ezy_95@outlook.com>
2022-04-04 13:25:06 +0800

    until jsx css

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Hello() {
-  return(
-    <h1>Hello World!</h1>
+// CSS
+import './index.css';
+
+const BookList = () => {
+  return (
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+const Book = () => {
+  return (
+    <article className='book'>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+}
+
+const Image = () => {
+  return (
+    <img src="https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL604_SR604,400_.jpg" alt="image load fail" />
+  );
+}
+
+const Title = () => {
+  return (
+    <h1>It's Not Easy Being a Bunny</h1>
+  );
+}
+
+const Author = () => {
+  return (
+    <h4 style={{ color: '#617d98' }}>Marilyn Sadler</h4>
+  );
+}
+
+ReactDOM.render(<BookList />, document.getElementById('root'));
--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.use(express.static('./client/build'))
+app.use(express.static('./client/build'));
 
 app.get('/generate', (req, res) => {
-	res.status(200).json({ generate: 'Successfull' })
-})
+	res.status(200).json({ generate: 'Successfull' });
+});
 
 app.get('/api/v1', (req, res) => {
-	res.status(200).json({ msg: 'This is API v1' })
-})
+	res.status(200).json({ msg: 'This is API v1' });
+});
 
 app.all('*', (req, res) => {
-	res.status(404).send('Not found')
-})
+	res.status(404).send('Not found');
+});
 
-const port = 5000
+const port = 5000;
 
 const start = () => {
-	app.listen(port, console.log(`Server is listening at port: ${port}`))
-}
+	app.listen(port, console.log(`Server is listening at port: ${port}`));
+};
 
-start()
+start();
