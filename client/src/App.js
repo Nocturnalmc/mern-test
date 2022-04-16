f52a450b4f959bbabc5bf9aa6eda89561dc37abd
Noct <ezy_95@outlook.com>
2022-04-16 10:44:14 +0800

    cleanup cra template

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
-import logo from './logo.svg';
-//import './App.css';
-import OpenClose from './OpenClose'
+import OpenClose from './OpenClose';
 
 function App() {
   return (
-		<>
-			<OpenClose />
-			<h1 className="text-3xl font-bold underline">
-				My first tailwind css !
-			</h1>
-   </>
+    <>
+      <OpenClose />
+      <h1 className='text-3xl font-bold underline'>My first tailwind css !</h1>
+    </>
   );
 }
 
