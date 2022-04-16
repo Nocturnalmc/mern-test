a8b74bdffb1ea7626133ddd58696462680c5ee1e
Noct <ezy_95@outlook.com>
2022-04-16 11:13:01 +0800

    add bg

--- client/src/OpenClose.js
+++ client/src/OpenClose.js
@@ -1,18 +1,23 @@
 function OpenClose() {
-	return (
-		<>
-			<div class="max-w-lg mx-auto p-8">
-  			<details class="open:bg-white dark:open:bg-slate-900 open:ring-1 open:ring-black/5 dark:open:ring-white/10 open:shadow-lg p-6 rounded-lg" close>
-    			<summary class="text-sm leading-6 text-slate-900 dark:text-white font-semibold select-none">
-      			Why do they call it Ovaltine?
-    			</summary>
-    			<div class="mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400">
-			      <p>The mug is round. The jar is round. They should call it Roundtine.</p>
-			    </div>
-			  </details>
-			</div>
-		</>
-	)
+  return (
+    <>
+      <div class='max-w-lg mx-auto p-8 bg-red-300 shadow-lg rounded-lg mt-5'>
+        <details
+          class='open:bg-white dark:open:bg-slate-900 open:ring-1 open:ring-black/5 dark:open:ring-white/10 open:shadow-lg p-6 rounded-lg'
+          close
+        >
+          <summary class='text-sm leading-6 text-slate-900 dark:text-white font-semibold select-none'>
+            Why do they call it Ovaltine?
+          </summary>
+          <div class='mt-3 text-sm leading-6 text-slate-600 dark:text-slate-400'>
+            <p>
+              The mug is round. The jar is round. They should call it Roundtine.
+            </p>
+          </div>
+        </details>
+      </div>
+    </>
+  );
 }
 
 export default OpenClose;
