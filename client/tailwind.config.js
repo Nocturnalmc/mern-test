9f45a7c9b06fcf932d2caa0abcf971677ddf8068
Noct <ezy_95@outlook.com>
2022-04-16 17:33:22 +0800

    try transition @ index.html

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   darkMode: 'class',
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./public/index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
