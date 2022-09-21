baa8211cf352e7a240384486c62dff240a2bd62e
noct <ezy_95@outlook.com>
2022-09-21 08:37:53 +0800

    changes @ testing-1

--- 17-6-2022/src/App.js
+++ 17-6-2022/src/App.js
@@ -17,7 +17,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React Yan ada changes sikit
+          Learn React Yan changes @ testing 1
         </a>
       </header>
     </div>
